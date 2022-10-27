8559bea456bd6c5f5314a15b0f085188cb8b3289
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-10-27 10:05:40 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 
-//Plugin By Xynoz!!
+//Plugin By F4Q Store!!
 let handler = async (m, { conn, usedPrefix }) => {
   const ultah = new Date('November 4 2022 00:00:01')
     const sekarat = new Date().getTime() 
@@ -9,23 +9,23 @@ let handler = async (m, { conn, usedPrefix }) => {
     const ojam = Math.floor( Kurang % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
     const onet = Math.floor( Kurang % (1000 * 60 * 60) / (1000 * 60))
     const detek = Math.floor( Kurang % (1000 * 60) / 1000)
-  let vn = `https://github.com/saipulanuar/Api-Github/raw/main/audio/Donasiku.mp3`
+  let vn = `https://github.com/XiaoyuOfc/RPG-MD-V4/blob/master/mp3/insekur.mp3`
   conn.sendButton(m.chat, `
 ┏━ꕥ〔 *Donasi • Dana* 〕ꕥ━⬣
-┃✾ Gopay  [088279268363]
-┃✾ Dana  [088279268363]
-┃✾ Saweria  [https://saweria.co/raraharsita2]
+┃✾ Pulsa  [ 081335334848 ]
+┃✾ Dana  [ 081335334848 ]
+┃✾ Saweria  [ Nothing ]
 ┗━━━━ꕥ
 ┏━━ꕥ〔 *NOTE* 〕ꕥ━⬣
-┃ Ingin donasi? Wa.me/6288279268363
+┃ Ingin donasi? Wa.me/6285831796696
 ┃ _Hasil donasi akan digunakan buat sewa_
 ┃ _atau beli *RDP/VPS* agar bot bisa jalan_
 ┃ _24jam tanpa kendala_
 ┗━━━━ꕥ
 `.trim(), wm, [['Owner',`/owner`]],m) // Tambah sendiri kalo mau
-conn.sendFile(m.chat, vn, 'haori.mp3', null, m, true, {
+conn.sendFile(m.chat, vn, '© By F4Q Store', null, m, true, {
 type: 'audioMessage', 
-ptt: true, contextInfo:{ externalAdReply: { title: `💌 Ultah Owner : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`, body: `Follow Tiktok My Bestie`, sourceUrl: 'https://www.tiktok.com/@raraharsita2', thumbnail: await (await fetch('https://i.ibb.co/jfZVKmC/babi2.jpg')).buffer(),}} 
+ptt: true, contextInfo:{ externalAdReply: { title: `Orang baik Pasti Mau donasi:v`, body: `Gak Maksa Kok Donasi Nya`, sourceUrl: 'https://www.tiktok.com/@anjay', thumbnail: await (await fetch('https://telegra.ph/file/7db49c83fd106a60741cd.jpg')).buffer(),}} 
      }) 
 }
 handler.command = /^(donasi|dns)$/i
@@ -36,6 +36,6 @@ handler.limit = false
 
 export default handler
 
-// 📮 Made In Xynoz 
-// Subscribe YouTube Xynoz!
+// 📮 Made In F4Q Store
+// Gosah Dihapus Anj
 // Tq To Jangan Di Hpus!!
