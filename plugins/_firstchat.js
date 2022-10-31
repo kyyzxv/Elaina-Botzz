b14fa2d5291e31a974dfbdea55a064c0e5d295f5
kyyzxv <116820327+kyyzxv@users.noreply.github.com>
2022-10-31 17:14:44 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -6,7 +6,7 @@ export async function before(m) {
     let user = global.db.data.users[m.sender]
     let txt = `👋Hai, ${ucapan()}
 
-${user.banned ? '📮Maaf, kamu dibanned & Tidak bisa menggunakan bot ini lagi' : `💬 Ada yg bisa 𝑽𝑿_𝑩𝑶𝑻𝒁 bantu?\nInfo: Premium kereset? chat owner segera!\n\n*CONTOH:*\nAyojoin https://chat.whatsapp.com/I5KoJMPEP6z19GZXQ7OpJ7\n\nAyo masuk`}`.trim()
+${user.banned ? '📮Maaf, kamu dibanned & Tidak bisa menggunakan bot ini lagi' : `💬 Ada yg bisa 𝑽𝑿_𝑩𝑶𝑻𝒁 bantu?\nInfo: Premium kereset? chat owner segera!\n\n*𝑽𝑿_𝑩𝑶𝑻𝒁:*\nAyojoin https://chat.whatsapp.com/I5KoJMPEP6z19GZXQ7OpJ7\n\nAyo masuk`}`.trim()
 
     if (new Date() - user.pc < 21600000) return // waktu ori 21600000 (6 jam)
     await this.sendButton(m.chat, txt, user.banned ? wm : '📮Note: Jangan spam bot nya', [user.banned ? 'MENU' : 'MENU', user.banned ? '.menu' : '.menu'], m)
