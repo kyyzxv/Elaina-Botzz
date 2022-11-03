cb272b81acc044ff53917708c68daee87f62ad1f
kyyzxv <116820327+kyyzxv@users.noreply.github.com>
2022-11-03 12:24:09 +0700

    Update info-listpc.js

--- plugins/info-listpc.js
+++ plugins/info-listpc.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
 let pc = Object.entries(await conn.chats)
 let niorg = pc.filter(([jid]) => jid.endsWith('@s.whatsapp.net'))
-let thumbListpc = `https://telegra.ph/file/d5a1cc2ab8af9bf5acf83.jpg`
+let thumbListpc = `https://telegra.ph/file/0c668d80b4942769ab9dc.jpg`
 let txt = ''
     for (let [jid] of niorg)
 txt += `${htjava} ${await conn.getName(jid)}\n${dmenub} ${'@' + jid.replace(/@.+/, '')}\n${cmenua}\n\n`
